67a09a34799a242dd8e1a151088b55d6eb3aa1ab
sygnator <mateusz@sygnator.com>
2024-01-28 22:59:21 +0100

    Add pre-commit configuration

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import streamlit as st
 import requests
 
 # count embeds
-if 'embed_counter' not in st.session_state:
+if "embed_counter" not in st.session_state:
     st.session_state.embed_counter = 0
+
 
 def send_discord_webhook(url, data):
     response = requests.post(url, json=data)
     return response
 
+
 def create_embed_fields():
     text_embeds = []
 
-    if st.button("Add embed"): 
+    if st.button("Add embed"):
         if st.session_state.embed_counter < 3:
             st.session_state.embed_counter += 1
         else:
@@ -21,16 +23,21 @@ def create_embed_fields():
     for i in range(st.session_state.embed_counter):
         with st.expander(f"Embed {i+1}"):
             e_title = st.text_input("Enter Embed Title", key=f"t{i}")
-            e_description = st.text_area(f"Enter Embed Content:", max_chars=2000, key=f"d{i}")
-            text_embeds.append({'title': e_title, 'description': e_description})
-    
+            e_description = st.text_area(
+                f"Enter Embed Content:", max_chars=2000, key=f"d{i}"
+            )
+            text_embeds.append({"title": e_title, "description": e_description})
+
     return text_embeds
+
 
 def main():
     st.title("Discord Webhook Customizer")
 
     # Input fields for user to customize the Discord webhook
-    webhook_url = st.text_input("Enter Discord Webhook URL:", placeholder="https://discord.com/api/webhooks/...")
+    webhook_url = st.text_input(
+        "Enter Discord Webhook URL:", placeholder="https://discord.com/api/webhooks/..."
+    )
     content = st.text_area(f"Enter Message Content:", max_chars=2000)
 
     # Add a new embed field
@@ -43,10 +50,7 @@ def main():
         if not webhook_url:
             st.warning("Please enter a Discord Webhook URL.")
         else:
-            data = {
-                "content": content,
-                "embeds": embeds
-            }
+            data = {"content": content, "embeds": embeds}
 
             response = send_discord_webhook(webhook_url.replace(" ", ""), data)
 
@@ -54,6 +58,7 @@ def main():
                 st.success("Message sent successfully!")
             else:
                 st.error(f"Failed to send message. Status Code: {response.status_code}")
+
 
 if __name__ == "__main__":
     main()
